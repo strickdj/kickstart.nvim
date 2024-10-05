8cfb903170c9adfcda71a252eb173c2a9a2d6b5c
Daris Strickland <daris.strickland@gmail.com>
2024-10-04 20:22:09 -0400

    added neotest and config

--- init.lua
+++ init.lua
@@ -909,12 +909,110 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
+
   {
     'rest-nvim/rest.nvim',
     dependencies = {
       {
         'nvim-neotest/nvim-nio',
         'j-hui/fidget.nvim',
+      },
+    },
+  },
+
+  {
+    'nvim-neotest/neotest',
+    dependencies = {
+      'nvim-neotest/nvim-nio',
+      'nvim-lua/plenary.nvim',
+      'antoinemadec/FixCursorHold.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      { 'fredrikaverpil/neotest-golang', version = '*' },
+    },
+    config = function()
+      require('neotest').setup {
+        adapters = {
+          require 'neotest-golang', -- Registration
+        },
+      }
+    end,
+    keys = {
+      {
+        '<leader>ta',
+        function()
+          require('neotest').run.attach()
+        end,
+        desc = '[t]est [a]ttach',
+      },
+      {
+        '<leader>tf',
+        function()
+          require('neotest').run.run(vim.fn.expand '%')
+        end,
+        desc = '[t]est run [f]ile',
+      },
+      {
+        '<leader>tA',
+        function()
+          require('neotest').run.run(vim.uv.cwd())
+        end,
+        desc = '[t]est [A]ll files',
+      },
+      {
+        '<leader>tS',
+        function()
+          require('neotest').run.run { suite = true }
+        end,
+        desc = '[t]est [S]uite',
+      },
+      {
+        '<leader>tn',
+        function()
+          require('neotest').run.run()
+        end,
+        desc = '[t]est [n]earest',
+      },
+      {
+        '<leader>tl',
+        function()
+          require('neotest').run.run_last()
+        end,
+        desc = '[t]est [l]ast',
+      },
+      {
+        '<leader>ts',
+        function()
+          require('neotest').summary.toggle()
+        end,
+        desc = '[t]est [s]ummary',
+      },
+      {
+        '<leader>to',
+        function()
+          require('neotest').output.open { enter = true, auto_close = true }
+        end,
+        desc = '[t]est [o]utput',
+      },
+      {
+        '<leader>tO',
+        function()
+          require('neotest').output_panel.toggle()
+        end,
+        desc = '[t]est [O]utput panel',
+      },
+      {
+        '<leader>tt',
+        function()
+          require('neotest').run.stop()
+        end,
+        desc = '[t]est [t]erminate',
+      },
+      {
+        '<leader>td',
+        function()
+          require('neotest').run.run { suite = false, strategy = 'dap' }
+        end,
+        desc = 'Debug nearest test',
       },
     },
   },
